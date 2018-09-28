f655236f2a1f7e4931c6e41d526bb6a26c31ab44
chm0dx <42707010+chm0dx@users.noreply.github.com>
2018-09-28 02:19:26 -0400

    Update fsGen.py

--- fsGen.py
+++ fsGen.py
@@ -4,6 +4,10 @@ import sys
 
 base = sys.argv[1]
 count = int(sys.argv[2])
+if(len(sys.argv) == 4):
+        size = int(sys.argv[3])
+        mode = "fixed"
+
 dirs = []
 dirs.append(base)
 dirCount = 0
@@ -11,36 +15,52 @@ fileCount= 0
 exts = ["txt","exe","jpg","docx","mp3","mp4"]
 
 def Roll():
-	if (random.randint(1,41) <= 3):
-		global dirCount
-		dirCount+=1
-		objType = "dir"
-	else:
-		global fileCount
-		fileCount+=1
-		objType = "file"
-	return objType
+        if (random.randint(1,41) <= 3):
+                global dirCount
+                dirCount+=1
+                objType = "dir"
+        else:
+                global fileCount
+                fileCount+=1
+                objType = "file"
+        return objType
 
 def makeDir(int,parent):
-	path = os.path.join(parent,str(int))
-	os.mkdir(path)
-	dirs.append(path)
+        path = os.path.join(parent,str(int))
+        os.mkdir(path)
+        dirs.append(path)
+
+def makeFileR(int,parent):
+        size = random.randint(1024,1024000)
+        if (size > 1022000):
+                size = random.randint(1024,1024000000)
+        path = os.path.join(parent,str(int) + "." + random.choice(exts))
+        f = open(path,"w")
+        f.write(os.urandom(size))
+        f.close()
 
 def makeFile(int,parent):
-	size = random.randint(1024,1024000)
-	if (size > 1022000):
-		size = random.randint(1024,1024000000)
-	path = os.path.join(parent,str(int) + "." + random.choice(exts))
-	f = open(path,"w")
-	f.write(os.urandom(size))
-	f.close()
+        path = os.path.join(parent,str(int) + "." + random.choice(exts))
+        f = open(path,"w")
+        f.write(os.urandom(size))
+        f.close()
 
-for i in range(1,count+1):
-	print i
-	parent = random.choice(dirs)
-	if (Roll() == "dir"):
-		makeDir(i,parent)
-	else:
-		makeFile(i,parent)
+if(mode == "random"):
+        for i in range(1,count+1):
+                print i
+                parent = random.choice(dirs)
+                if (Roll() == "dir"):
+                        makeDir(i,parent)
+                else:
+                        makeFileR(i,parent)
+
+else:
+        for i in range(1,count+1):
+                print i
+                parent = random.choice(dirs)
+                if (Roll() == "dir"):
+                        makeDir(i,parent)
+                else:
+                        makeFile(i,parent)
 
 print str(fileCount) + " Files\r\n" + str(dirCount) + " Directories"
